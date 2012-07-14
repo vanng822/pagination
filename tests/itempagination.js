7b798df1539b090849bc70b11ac740593066de04
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2012-07-14 11:34:27 +0200

    Adding ItemPagination rendering.

--- tests/itempagination.js
+++ tests/itempagination.js
@@ -6,6 +6,15 @@ function test() {
 	console.log('Running ItemPaginator test');
 	/* pageLinks 5 by default; howto specify for Item Pattern */
 	
+	var item = new ItemPaginator({
+		prelink : '/',
+		pageLinks : 5,
+		current : 5,
+		totalResult : 98
+	});
+	
+	assert.equal('<div class="paginator"><span class="paginator-current-report">Results 41 - 50 of 98</span><a href="/?page=1" class="paginator-first">First</a><a href="/?page=4" class="paginator-previous">Previous</a><a href="/?page=6" class="paginator-next">Next</a><a href="/?page=10" class="paginator-last">Last</a></div>', item.render());
+
 }
 
 test();
--- tests/translator.js
+++ tests/translator.js
@@ -1,12 +1,16 @@
 var assert = require('assert');
 var pagination = require('../index.js');
 var SearchPaginator = pagination.SearchPaginator;
+var ItemPaginator = pagination.ItemPaginator
 
 function test() {
 	console.log('Running translation test');
 	var translations = {
-		'Previous': 'Voorgaand',
-		'Next':'Volgende'
+		'PREVIOUS' : 'Voorgaand',
+		'NEXT' : 'Volgende',
+		'FIRST' : 'Eerst',
+		'LAST' : 'Laatste',
+		'CURRENT_PAGE_REPORT' : 'Resulten {FromResult} - {ToResult} van {TotalResult}'
 	};
 	var item = new SearchPaginator({
 		prelink : '/',
@@ -17,10 +21,46 @@ function test() {
 			return translations[str];
 		}
 	});
-	
-	assert.equal('<div class="paginator"><a href="/?page=4" class="paginator-previous">Voorgaand</a><a href="/?page=3" class="paginator-page">3</a><a href="/?page=4" class="paginator-page">4</a><a href="/?page=5" class="paginator-current">5</a><a href="/?page=6" class="paginator-page">6</a><a href="/?page=7" class="paginator-page">7</a><a href="/?page=6" class="paginator-next">Volgende</a></div>', item.render());
 
+	assert.equal('<div class="paginator"><a href="/?page=4" class="paginator-previous">Voorgaand</a><a href="/?page=3" class="paginator-page paginator-page-first">3</a><a href="/?page=4" class="paginator-page">4</a><a href="/?page=5" class="paginator-current">5</a><a href="/?page=6" class="paginator-page">6</a><a href="/?page=7" class="paginator-page paginator-page-last">7</a><a href="/?page=6" class="paginator-next">Volgende</a></div>', item.render());
+	
+	var item = new ItemPaginator({
+		prelink : '/',
+		pageLinks : 5,
+		current : 5,
+		totalResult : 100,
+		translator : function(str) {
+			return translations[str];
+		}
+	});
+
+	assert.equal('<div class="paginator"><span class="paginator-current-report">Resulten 41 - 50 van 100</span><a href="/?page=1" class="paginator-first">Eerst</a><a href="/?page=4" class="paginator-previous">Voorgaand</a><a href="/?page=6" class="paginator-next">Volgende</a><a href="/?page=10" class="paginator-last">Laatste</a></div>', item.render());
 
 }
 
+
+function testMarkup() {
+	console.log('Running translation test, markup');
+	var translations = {
+		'PREVIOUS' : '<span class="left-arrow"></span><span class="text">Voorgaand</span>',
+		'NEXT' : '<span class="right-arrow"></span><span class="text">Volgende</span>',
+		'FIRST' : 'Eerst',
+		'LAST' : 'Laatste',
+		'CURRENT_PAGE_REPORT' : 'Resulten {FromResult} - {ToResult} van {TotalResult}'
+	};
+	var item = new SearchPaginator({
+		prelink : '/',
+		pageLinks : 5,
+		current : 5,
+		totalResult : 100,
+		translator : function(str) {
+			return translations[str];
+		}
+	});
+
+	assert.equal('<div class="paginator"><a href="/?page=4" class="paginator-previous"><span class="left-arrow"></span><span class="text">Voorgaand</span></a><a href="/?page=3" class="paginator-page paginator-page-first">3</a><a href="/?page=4" class="paginator-page">4</a><a href="/?page=5" class="paginator-current">5</a><a href="/?page=6" class="paginator-page">6</a><a href="/?page=7" class="paginator-page paginator-page-last">7</a><a href="/?page=6" class="paginator-next"><span class="right-arrow"></span><span class="text">Volgende</span></a></div>', item.render());
+}
+
+
 test();
+testMarkup();
