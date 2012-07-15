473669f0f149c68690a50fd2e8b9840390f6791e
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2012-07-15 16:29:31 +0200

    Fix rendering of Item pagination. The control buttons should switch on and off in this pattern.

--- tests/itempagination.js
+++ tests/itempagination.js
@@ -15,6 +15,23 @@ function test() {
 	
 	assert.equal('<div class="paginator"><span class="paginator-current-report">Results 41 - 50 of 98</span><a href="/?page=1" class="paginator-first">First</a><a href="/?page=4" class="paginator-previous">Previous</a><a href="/?page=6" class="paginator-next">Next</a><a href="/?page=10" class="paginator-last">Last</a></div>', item.render());
 
+	var item = new ItemPaginator({
+		prelink : '/',
+		pageLinks : 5,
+		current : 1,
+		totalResult : 98
+	});
+	
+	assert.equal('<div class="paginator"><span class="paginator-current-report">Results 1 - 10 of 98</span><span class="paginator-first">First</span><span class="paginator-previous">Previous</span><a href="/?page=2" class="paginator-next">Next</a><a href="/?page=10" class="paginator-last">Last</a></div>', item.render());
+
+	var item = new ItemPaginator({
+		prelink : '/',
+		pageLinks : 5,
+		current : 10,
+		totalResult : 98
+	});
+	assert.equal('<div class="paginator"><span class="paginator-current-report">Results 91 - 98 of 98</span><a href="/?page=1" class="paginator-first">First</a><a href="/?page=9" class="paginator-previous">Previous</a><span class="paginator-next">Next</span><span class="paginator-last">Last</span></div>', item.render());
+
 }
 
 test();
