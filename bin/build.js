43f7c4986d49c62cb7053573bb34f87d74860e38
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-02-22 12:51:20 +0100

    Adding build script and build different versions so we choose for browser.

---
+++ bin/build.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+var fs = require('fs');
+var program = require('commander');
+var uglifyJs = require('uglify-js');
+var pkgs = [];
+var releaseContent = [];
+program
+	.option('-o, --output <path>', 'Output file', String)
+	.option('-e, --template-engine', 'Include template engine')
+	.option('-i, --item', 'Include ItemPaginator')
+	.option('-s, --search', 'Include SearchPaginator')
+	.option('-t, --template', 'Include TemplatePaginator')
+	//.option('-r, --release', 'Building release versions')
+	.option('-a, --all', 'Include all: Template Engine, ItemPaginator, SearchPaginator, TemplatePaginator')
+	.parse(process.argv);
+
+if (program.all) {
+	pkgs.push('template');
+	pkgs.push('item_paginator');
+	pkgs.push('search_paginator');
+	pkgs.push('template_paginator');
+} else {
+	if (program.templateEngine) {
+		pkgs.push('template');
+	}
+	if (program.item) {
+		pkgs.push('item_paginator');
+	}
+	if (program.search) {
+		pkgs.push('search_paginator');
+	}
+	if (program.template) {
+		pkgs.push('template_paginator');
+	}
+}
+
+releaseContent.push(fs.readFileSync('./lib/pagination.js'));
+pkgs.forEach(function(m) {
+	releaseContent.push(fs.readFileSync('./lib/' + m + '.js'));
+	releaseContent.push('exports.module(pagination, pagination.util);');
+});
+var minified = uglifyJs.minify(releaseContent.join(";"), {fromString: true});
+
+if (program.output) {
+	fs.writeFileSync(program.output, minified.code);
+} else {
+	console.log(minified.code);
+}
+
