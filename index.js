3baac7bf2eb54a7dd361430e51ab1c190a823d6a
Ramesh Nair <ram@hiddentao.com>
2015-09-02 12:20:27 +0800

    Fix for browserify
    Change require() calls so that browserify can detect the includes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var pkgs = ['item_paginator', 'search_paginator', 'template_paginator', 'template'];
 var pagination = require('./lib/pagination.js');
 var util = require('util');
-var i, len, pkg;
-for (i = 0, len = pkgs.length; i < len; i++) {
-	pkg = require('./lib/' + pkgs[i]  + '.js');
-	pkg.module(pagination, util);
-}
+require('./lib/item_paginator')(pagination, util);
+require('./lib/search_paginator')(pagination, util);
+require('./lib/template_paginator')(pagination, util);
+require('./lib/template')(pagination, util);
 
 module.exports = pagination;
